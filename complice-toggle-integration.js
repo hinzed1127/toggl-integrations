b15091b1487438037d844c09e4380912025aca0e
Dan Hinze <hinzed1127@gmail.com>
2022-01-19 16:51:34 -0600

    Run prettier

--- complice-toggle-integration.js
+++ complice-toggle-integration.js
@@ -1,43 +1,42 @@
-'use strict';
+"use strict"
 
 // Main function. Set small timeout to let tasks/projects load.
-setTimeout(() => {
-    togglbutton.render(
-    '.nowdothis .ndt-text:not(.toggl)',
-    { observe: true },
-    function (elem) {
-        const link = togglbutton.createTimerLink({
-        description: getTaskText(),
-        projectName: getProjectName(),
-        tags: ['Complice']
-        });
-        elem.appendChild(link);
-        link.addEventListener('click', () => {
-            link.remove();
-            // Fix modal styling bug this causes
-            setTimeout(() =>{
-                const modal = document.querySelector('#toggl-button-edit-form')
-                modal.remove()
-            }, 500);
-        })      
-    }
-)}, 1000);
+setTimeout(() => {}, 1000)
+
+function createButton() {
+  togglbutton.render(".nowdothis .ndt-text:not(.toggl)", { observe: true }, function (elem) {
+    const link = togglbutton.createTimerLink({
+      description: getTaskText(),
+      projectName: getProjectName(),
+      tags: ["Complice"],
+    })
+    elem.appendChild(link)
+    link.addEventListener("click", () => {
+      link.remove()
+      // Fix modal styling bug this causes
+      setTimeout(() => {
+        const modal = document.querySelector("#toggl-button-edit-form")
+        modal.remove()
+      }, 500)
+    })
+  })
+}
 
 const getTaskText = () => {
-  const taskText = document.querySelector('.nowdothis .ndt-text').innerText
-  return taskText.substring(taskText.lastIndexOf(')') + 1).trim()
+  const taskText = document.querySelector(".nowdothis .ndt-text").innerText
+  return taskText.substring(taskText.lastIndexOf(")") + 1).trim()
 }
 
 const getTaskGoal = () => {
-    const taskText = document.querySelector('.nowdothis .ndt-text').innerText
-    const goalNumber = taskText.indexOf('(') == -1 ? taskText[0] : taskText[1]
+  const taskText = document.querySelector(".nowdothis .ndt-text").innerText
+  const goalNumber = taskText.indexOf("(") == -1 ? taskText[0] : taskText[1]
 
-    return goalNumber
+  return goalNumber
 }
 
 const getProjectName = () => {
-  const goals = Array.from(document.querySelectorAll('.goalsbar-name'))
-  const goalNames = goals.map(goal => goal.innerText)
+  const goals = Array.from(document.querySelectorAll(".goalsbar-name"))
+  const goalNames = goals.map((goal) => goal.innerText)
   // subtract one for proper array index
-  return goalNames[getTaskGoal() - 1];
+  return goalNames[getTaskGoal() - 1]
 }
